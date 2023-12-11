a68ffa7ea874b8d2f211b4aea60c765a8dbfd216
carolgsomnio <carglassm@gmail.com>
2023-12-10 21:21:18 -0300

    to lower case messages

--- lib/utils/bot_response.dart
+++ lib/utils/bot_response.dart
@@ -1,17 +1,17 @@
 /// This function returns a bot response based on the message sent by the user.
 String getBotResponse(String message) {
-  switch (message) {
-    case 'Hello':
+  switch (message.toLowerCase()) {
+    case 'hello':
       return 'Hello, how can I help?';
-    case 'Thank you!':
+    case 'thank you!':
       return "You're welcome!";
-    case 'See you later!':
+    case 'see you later!':
       return 'Hope to see you soon!';
-    case 'Good morning':
+    case 'good morning':
       return 'Good morning to you!';
-    case 'Good afternoon':
+    case 'good afternoon':
       return 'Good afternoon to you!';
-    case 'What are your functions?':
+    case 'what are your functions?':
       return 'I can help you chat, send pictures, and more!';
     default:
       return 'Message received!';
